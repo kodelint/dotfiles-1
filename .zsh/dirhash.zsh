d3ecbff45d4aebf185d105feb9552c494cf76d62
Koichi Shiraishi <k@zchee.io>
2016-06-23 00:48:27 +0900

    zsh/dirhash: Cleanup
    Signed-off-by: Koichi Shiraishi <k@zchee.io>

--- .zsh/dirhash.zsh
+++ .zsh/dirhash.zsh
@@ -2,17 +2,18 @@ hash -d zsh=$HOME/.zsh/
 
 # Go
 hash -d go=/usr/local/go/
-hash -d gosrc=$GOPATH/src/github.com/zchee/
-hash -d go-sandbox=$HOME/go/src/github.com/zchee/go-sandbox
+hash -d gosrc=/usr/local/go/src
+hash -d gopath=$GOPATH/src/github.com/zchee
+hash -d gosandbox=$HOME/go/src/github.com/zchee/go-sandbox
 
 # user local
-hash -d ulocal=/usr/local/
-hash -d bin=/usr/local/bin/
-hash -d brew=/usr/local/opt/
-hash -d include=/usr/local/include/
-hash -d lib=/usr/local/lib/
-hash -d opt=/usr/local/opt/
-hash -d share=/usr/local/share/
-hash -d var=/usr/local/var/
+hash -d ulocal=/usr/local
+hash -d bin=/usr/local/bin
+hash -d brew=/usr/local/opt
+hash -d include=/usr/local/include
+hash -d lib=/usr/local/lib
+hash -d opt=/usr/local/opt
+hash -d share=/usr/local/share
+hash -d var=/usr/local/var
 
-hash -d nvim=$HOME/.nvim/
+hash -d nvim=$HOME/.nvim
