0b5ac7632c67f274d19a5913b52a5c853119dd12
Koichi Shiraishi <k@zchee.io>
2016-06-23 00:54:01 +0900

    zsh/modules: Cleanup git module
    Signed-off-by: Koichi Shiraishi <k@zchee.io>

--- .zsh/modules/git.zsh
+++ .zsh/modules/git.zsh
@@ -2,46 +2,51 @@
 # Defines Git aliases.
 #
 
-
-# Format settings
-# _git_log_medium_format='%C(bold)Commit:%C(reset) %C(green)%H%C(red)%d%n%C(bold)Author:%C(reset) %C(cyan)%an <%ae>%n%C(bold)Date:%C(reset)   %C(blue)%ai (%ar)%C(reset)%n%+B'
-_git_log_oneline_format='%C(green)%h%C(reset) %s%C(red)%d%C(reset)%n'
-_git_log_brief_format='%C(green)%h%C(reset) %s%n%C(blue)(%ar by %an)%C(red)%d%C(reset)%n'
-
-
-#
 # Aliases
-#
+alias g='git'
+alias h='hub'
 
-# Git & hub
-alias g='hub'
-# if type hub >/dev/null 2>&1; then
-#   function git(){hub "$@"}
-# fi
+# }}}
+###################################################################################################
 
+# TOC {{{
 
-# Lists
-#  - Branch
-#  - Commit
-#  - Conflict
-#  - Diff
-#  - Fetch
-#  - Grep
-#  - Index
-#  - Log
-#  - ls-files
-#  - Merge
-#  - Push
-#  - Rebase
-#  - Remote
-#  - Stash
-#  - Status
-#  - Submodule
-#  - Working copy
+# - Custom
+# - (b)ranch
+# - (c)ommit
+# - (C)onflict
+# - (di)ff
+# - (f)etch
+# - (g)rep
+# - (i)ndex
+# - (l)og
+# - (i)index (l)s-files
+# - (m)erge
+# - (p)ush
+# - (r)ebase
+# - (r)emote
+# - (S)tash
+# - (su)bmodule
+# - (w)orking Copy
+# - (g)arbage (c)ollection
 
+# }}}
+###################################################################################################
 
-# (b)ranch
-alias  gba='git branch'
+# Custom {{{
+
+alias gfa='git fetch --all'
+alias gpm='git pull origin $(git rev-parse --abbrev-ref HEAD)'
+alias grt='cd $(git rev-parse --show-toplevel)'
+alias gs='git status --ignore-submodules=none'
+alias gs.='git status --ignore-submodules=none .'
+
+# }}}
+###################################################################################################
+
+# (b)ranch {{{
+
+alias gba='git branch'
 alias gbc='git checkout -b'
 alias gbL='git branch --all --verbose'
 alias gbl='git branch --verbose'
@@ -52,12 +57,16 @@ alias gbsall='git show-branch --all'
 alias gbx='git branch --delete'
 alias gbX='git branch --delete --force'
 
-# (c)ommit
-alias gc='git commit --verbose --signoff' # (C)ommit with show all change log
-alias gca='git commit --all --verbose --signoff' # (C)ommit (a)ll changes
+# }}}
+###################################################################################################
+
+# (c)ommit {{{
+
+alias gc='git commit --signoff' # (C)ommit with show all change log
+alias gca='git commit --all --signoff' # (C)ommit (a)ll changes
       gce() { git commit --message "$1"; } # (C)ommit with surround in double quotes the m(e)ssage automatically
 alias gcf='git commit --amend --reuse-message HEAD'
-alias gcF='git commit --verbose --amend'
+alias gcF='git commit --amend'
 # alias gcl='git-commit-lost'
 alias gcO='git checkout --patch'
 alias gco='git checkout'
@@ -67,7 +76,11 @@ alias gcR='git reset "HEAD^"'
 alias gcr='git revert'
 alias gcs='git show'
 
-# (C)onflict
+# }}}
+###################################################################################################
+
+# (C)onflict {{{
+
 alias gCl='git status | sed -n "s/^.*both [a-z]*ed: *//p"'
 alias gCa='git add $(gCl)'
 alias gCe='git mergetool $(gCl)'
@@ -76,19 +89,34 @@ alias gCO='gCo $(gCl)'
 alias gCt='git checkout --theirs --'
 alias gCT='gCt $(gCl)'
 
-# Diff (di)ff
+# }}}
+###################################################################################################
+
+# (di)ff {{{
+
 alias gd='git diff'
 alias gdi='git diff'
-alias gdiw='git diff --no-ext-diff'
-alias gdiW='git diff --no-ext-diff --word-diff'
+alias gd.='git diff .'
+alias gdw='git diff --no-ext-diff'
+alias gdW='git diff --no-ext-diff --word-diff'
 
-# Fetch (f)
+# }}}
+###################################################################################################
+
+# (f)etch {{{
+
 alias gf='git fetch'
 alias gfa='git fetch --all'
+
 alias gcl='git clone'
-alias gfc='git clone'
+alias gcn='git clone'
+
 alias gfr='git pull --rebase'
 
+# }}}
+###################################################################################################
+
+# (g)it (f)etch (p)ull request
 # http://qiita.com/tarr1124/items/d807887418671adbc46f
 gfp() {
   USER_BRANCH=$2
@@ -97,9 +125,12 @@ gfp() {
   git branch --move $USER_BRANCH[2] pull/"$1"/"$USER_BRANCH[1]"/"$USER_BRANCH[2]"
   unset USER_BRANCH
 }
-      # gfp() { git fetch origin pull/"$1"/head:"$2"; git branch --move "$2" "pull/$1/$2" }
 
-# Grep (g)
+# }}}
+###################################################################################################
+
+# (g)rep {{{
+
 # alias gg='git grep'
 # alias ggi='git grep --ignore-case'
 # alias ggl='git grep --files-with-matches'
@@ -107,22 +138,30 @@ gfp() {
 # alias ggv='git grep --invert-match'
 # alias ggw='git grep --word-regexp'
 
-# Index (i)
+# }}}
+###################################################################################################
+
+# (i)ndex {{{
+
 alias gia='git add'
-alias giA='git add --patch'
+alias giap='git add --patch'
 alias giu='git add --update'
-alias gid='git diff --no-ext-diff --cached'
 alias giD='git diff --no-ext-diff --cached --word-diff'
 alias gir='git reset'
 alias giR='git reset --patch'
 alias gix='git rm -r --cached'
 alias giX='git rm -rf --cached'
 
-# Log (l)
-# Default git log style
-# alias gl='git log --topo-order --stat --full-diff --graph --color --decorate=full --pretty=format:"${_git_log_medium_format}"'
-alias gl='git log --topo-order --stat --full-diff --graph --color --decorate=full'
+# }}}
+###################################################################################################
 
+# (l)og {{{
+
+# Format settings
+_git_log_oneline_format='%C(green)%h%C(reset) %s%C(red)%d%C(reset)%n'
+_git_log_brief_format='%C(green)%h%C(reset) %s%n%C(blue)(%ar by %an)%C(red)%d%C(reset)%n'
+
+alias gl='git log --topo-order --stat --full-diff --graph --color --decorate=full'
 alias gla='gl --all'                                                                        # Display all refs in refs/
 alias glb='git log --topo-order --pretty=format:"${_git_log_brief_format}"'                 # Brief format(need?)
 alias glc='git shortlog --summary --numbered'                                               # git commit summary
@@ -132,7 +171,11 @@ alias glo='git log --topo-order --pretty=format:"${_git_log_oneline_format}"'   
 alias glsig='gl --show-signature'                                                           # Show sign signature
 alias glsm='git log --topo-order --pretty=format:"${_git_log_medium_format}"'               # Simple
 
-# ls-files (g(i)t (ls))
+# }}}
+###################################################################################################
+
+# (i)index (l)s-files {{{
+
 # alias gildi='git status --porcelain --short --ignored | sed -n "s/^!! //p"'
 # alias gilk='git ls-files --killed'
 # alias gilm='git ls-files --modified'
@@ -141,14 +184,22 @@ alias glsm='git log --topo-order --pretty=format:"${_git_log_medium_format}"'   
 # alias gilu='git ls-files --other --exclude-standard'
 # alias gilx='git ls-files --deleted'
 
-# Merge (m)
+# }}}
+###################################################################################################
+
+# (m)erge {{{
+
 alias gm='git merge'
 alias gma='git merge --abort'
 alias gmC='git merge --no-commit'
 alias gmF='git merge --no-ff'
 alias gmt='git mergetool'
 
-# Push (p)
+# }}}
+###################################################################################################
+
+# (p)ush {{{
+
 alias gp='git push'
 alias gpA='git push --all && git push --tags'
 alias gpa='git push --all'
@@ -157,14 +208,22 @@ alias gpF='git push --force'
 alias gpp='git pull origin "$(git-branch-current 2> /dev/null)" && git push origin "$(git-branch-current 2> /dev/null)"'
 alias gpt='git push --tags'
 
-# Rebase (r)
-alias gr='git rebase'
+# }}}
+###################################################################################################
+
+# (r)ebase {{{
+
+alias gr='git rebase --autostash'
 alias gra='git rebase --abort'
 alias grc='git rebase --continue'
 alias gri='git rebase --interactive'
 alias grs='git rebase --skip'
 
-# Remote (R)
+# }}}
+###################################################################################################
+
+# (r)emote {{{
+
 alias gre='git remote --verbose'
 alias grea='git remote add'
 alias greb='git-hub-browse'
@@ -173,7 +232,11 @@ alias gres='git remote set-url'
 alias greu='git remote update'
 alias grex='git remote remove'
 
-# Stash (s)
+# }}}
+###################################################################################################
+
+# (S)tash {{{
+
 alias gS='git stash'
 alias gSa='git stash apply'
 alias gSd='git stash show --patch --stat'
@@ -187,13 +250,11 @@ alias gSw='git stash save --include-untracked --keep-index'
 alias gSx='git stash drop'
 alias gSX='git-stash-clear-interactive'
 
-# Status
-alias gfa='git fetch --all'
-alias gpm='git pull origin master'
-alias grt='cd $(git rev-parse --show-toplevel)'
-alias gs='git status --ignore-submodules=none'
+# }}}
+###################################################################################################
 
-# (su)bmodule
+# (su)bmodule {{{
+
 alias gsu='git submodule'
 alias gsua='git submodule add'
 alias gsuf='git submodule foreach'
@@ -205,7 +266,11 @@ alias gsus='git submodule sync'
 alias gsuu='git submodule foreach git pull origin master'
 alias gsux='git-submodule-remove'
 
-# Working Copy (w)
+# }}}
+###################################################################################################
+
+# (w)orking Copy
+
 alias gwC='git clean -f'
 alias gwc='git clean -n'
 alias gwR='git reset --hard'
@@ -214,6 +279,13 @@ alias gws='git status --ignore-submodules=${_git_status_ignore_submodules} --sho
 alias gwS='git status --ignore-submodules=${_git_status_ignore_submodules}'
 alias gwx='git rm -r'
 alias gwX='git rm -rf'
+
+# }}}
+###################################################################################################
+
+# (g)arbage (c)ollection
+alias ggc='git gc'
+alias gccf='git gc --prune=all'
 
 # }}}
 ####################################################################################################
