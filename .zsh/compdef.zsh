dde010f914fcaaa49f4e8c4cc2dd0418447e621b
Koichi Shiraishi <zchee.io@gmail.com>
2016-09-25 02:42:26 +0900

    compdef: add compdef.zsh
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ .zsh/compdef.zsh
@@ -0,0 +1,19 @@
+# ----------------------------------------------------------------------------
+# compdef
+
+# macOS default
+compdef _log log
+
+# nvim
+compdef _nvim n
+
+# _gnu_generic
+compdef \
+  _gnu_generic \
+    flex \
+    ctags \
+    dlv \
+    peco \
+    llvm-objdump \
+    pkgutil \
+    mypy
